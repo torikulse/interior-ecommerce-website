2b5e8a460e5ea668d32dd1ea6c0bd6fc5198e9b5
Torikul <devtorikul@gmail.com>
2025-01-28 00:07:06 +0600

    good night 01/27/2025

---
+++ experiment.js
@@ -0,0 +1,11 @@
+// let range = 7;
+// for (let i = 0; i < 20; i++) {
+//   let nextIndex = i % range;
+//   let nextIndex2 = (i + 1) % range;
+//   console.log(nextIndex);
+//   console.log(nextIndex2);
+// }
+
+
+
+
