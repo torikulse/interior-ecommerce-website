834c9c8c309dc891418ec0145d51745afba03216
Torikul <devtorikul@gmail.com>
2025-01-29 08:42:11 +0600

    1/28/2025 updated

--- javaScript.js
+++ javaScript.js
@@ -15,54 +15,11 @@ function menuHeader() {
 }
 menuHeader();
 
-//slider data
-
-let sliderData = [
-  { id: 1, name: "Bed Room", link: "https://www.torikulse.vercel.app" },
-  { id: 2, name: "Dining Room", link: "https://www.torikulse.vercel.app" },
-  { id: 3, name: "Living Room", link: "https://www.torikulse.vercel.app" },
-  { id: 4, name: "Room Four", link: "https://www.torikulse.vercel.app" },
-  { id: 5, name: "Room Five", link: "https://www.torikulse.vercel.app" },
-  { id: 6, name: "Room Six", link: "https://www.torikulse.vercel.app" },
-  { id: 7, name: "Room Seven", link: "https://www.torikulse.vercel.app" },
-];
-// //slider right
-// function rightArrow() {
-//   let active = document.querySelector(".slider .active");
-//   let active2 = document.querySelector(".slider .active2");
-//   if (active2.nextElementSibling) {
-//     active.classList.remove("active");
-//     active.nextElementSibling.classList.add("active");
-//     active2.classList.remove("active2");
-//     active2.nextElementSibling.classList.add("active2");
-//     console.log(active2.nextElementSibling);
-//   } else {
-//     active.classList.remove("active");
-//     active.nextElementSibling.classList.add("active");
-//     active2.classList.remove("active2");
-//     console.log(active.nextElementSibling);
-//   }
-// }
-// //slider left
-// function leftArrow() {
-//   let active = document.querySelector(".slider .active");
-//   let active2 = document.querySelector(".slider .active2");
-
-//   if (active.previousElementSibling) {
-//     active.classList.remove("active");
-//     active.previousElementSibling.classList.add("active");
-//     active2.classList.remove("active2");
-//     active2.previousElementSibling.classList.add("active2");
-//   } else {
-//     active2.classList.remove("active2");
-//   }
-// }
-// //end slider
-
+//slider right
 let card = document.querySelectorAll(".slider .card");
 let range = card.length;
 let i = 700;
-
+//params will be "next" or "prev"
 function slide(params) {
   if (params == "next") {
     i++;
@@ -85,6 +42,4 @@ function circleSlider(props) {
 
   card[props[0]].classList.add("active");
   card[props[1]].classList.add("active2");
-
-  console.log(props);
 }
